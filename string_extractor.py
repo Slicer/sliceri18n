4af22e1e096458d9e7bf9376447fe700ef835d49
syrjay <sy.jroumar@gmail.com>
2022-04-04 17:28:30 +0000

    new update

--- string_extractor.py
+++ string_extractor.py
@@ -1,7 +1,6 @@
 import ast
 import os
 
-# tree = ast.parse(code)
 scriptname = os.path.basename(__file__)
 scriptlocation = os.path.realpath(__file__)
 
@@ -19,6 +18,7 @@ def walk (node):
 
 def extractor(myfile):
   text_extracted=""
+  line_of_text=0
   with open(myfile, "r") as code:
     tree = ast.parse(code.read())
   with open("tsfile.ts", "w") as info:
@@ -35,27 +35,43 @@ def extractor(myfile):
         if isinstance(node.func, ast.Name):
             if node.func.id == "_":
               text_extracted = node.args[0].value
+              line_of_text = node.lineno
               print(text_extracted, " ligne ", node.lineno)
+              info.write("""
+              <message>
+                  <location filename='"""+scriptlocation+"""' line='"""+str(line_of_text)+"""'/>
+                  <source>"""+text_extracted+"""</source>
+                  <translation type="unfinished">"""+"""</translation>
+              </message>
+              """)
             elif len(node.args)==1 and isinstance(node.args[0], ast.Call) and isinstance(node.args[0].func, ast.Name):
               if node.args[0].func.id=="_":
                 text_extracted = node.args[0].args[0].value
-            print(text_extracted, " ligne ", node.lineno)
+                line_of_text = node.lineno
+                print(text_extracted, " ligne ", node.lineno)
+                info.write("""
+                <message>
+                    <location filename='"""+scriptlocation+"""' line='"""+str(line_of_text)+"""'/>
+                    <source>"""+text_extracted+"""</source>
+                    <translation type="unfinished">"""+"""</translation>
+                </message>
+                """)
         # if the node is an Attribute and the call node of the _() function is in this node
-      if isinstance(node.func, ast.Attribute) and len(node.args)>=1: 
-        if isinstance(node.args[0], ast.Call) and isinstance(node.args[0].func, ast.Name):
+        if isinstance(node.func, ast.Attribute) and len(node.args)>=1: 
+          if isinstance(node.args[0], ast.Call) and isinstance(node.args[0].func, ast.Name):
             if node.args[0].func.id=="_":
               text_extracted = node.args[0].args[0].value
+              line_of_text = node.lineno
               print(text_extracted, " ligne ", node.lineno)
-        line_of_text = str(node.lineno)
-        info.write("""
-        <message>
-            <location filename='"""+scriptlocation+"""' line='"""+line_of_text+"""'/>
-            <source>"""+text_extracted+"""</source>
-            <translation type="unfinished">"""+"""</translation>
-        </message>
-        """)
+              info.write("""
+              <message>
+                  <location filename='"""+scriptlocation+"""' line='"""+str(line_of_text)+"""'/>
+                  <source>"""+text_extracted+"""</source>
+                  <translation type="unfinished">"""+"""</translation>
+              </message>
+              """)
     info.write("""
         </context>
       </TS>
     """)
-# extractor("your_python_file.py")
+extractor("DICOMExportScene.py")
