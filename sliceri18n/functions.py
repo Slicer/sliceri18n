5d741d505afd7d12705eb61ec68b8b95a52064c1
Mouhamed DIOP <mouhamed.diop@esp.sn>
2023-03-30 19:09:54 +0000

    ENH: Handle many translation contexts in ts file generation
    See Slicer/sliceri18n#4
    See Slicer/SlicerLanguagePacks#8

--- sliceri18n/functions.py
+++ sliceri18n/functions.py
@@ -3,7 +3,7 @@ import ast
 import os
 import pathlib
 
-  
+
 def code_to_dict(source_file):
   """ Transform source code (extracted strings, line numbers,  location) 
   to dictionnary after parsing it with ast module. """
@@ -46,46 +46,50 @@ def code_to_dict(source_file):
           })
   return words_table
 
-def dict_to_ts(dico, source_file):
-  """ generate .ts file contaning extracted strings 
-  with the resultant dictionary
-  """
-  context_name = get_context(source_file)
-  tsfile_start_template = """<?xml version="1.0" encoding="utf-8"?>
-      <!DOCTYPE TS>
-      <TS version="2.1">
-        <context>
-          <name>"""+context_name+"""</name>"""
-  tsfile_end_template = """
-        </context>
-      </TS>"""
-  if not is_big(dico):
-    with open(context_name+".ts", "w") as f:
-      f.write(tsfile_start_template)
-      for msg in dico.values():
-        for msg_item in msg:
-          message_template = """
-            <message>
-                <location filename='"""+msg_item['filename']+"""' line='"""+str(msg_item['line'])+"""'/>
-                <source>"""+msg_item['source']+"""</source>
-                <translation type="unfinished">"""+"""</translation>
-            </message>"""
-          f.write(message_template)
-      f.write(tsfile_end_template)
-  else:
-    with open(context_name+".ts", "w") as f:
-      f.write(tsfile_start_template)
-      for minidict in dico.values():
-        for msg in minidict.values():
-          for msg_item in msg:
-            message_template = """
-              <message>
-                  <location filename='"""+msg_item['filename']+"""' line='"""+str(msg_item['line'])+"""'/>
-                  <source>"""+msg_item['source']+"""</source>
-                  <translation type="unfinished">"""+"""</translation>
-              </message>"""
-            f.write(message_template)
-      f.write(tsfile_end_template)
+def ts_context_item(context_name, source_file, dico):
+  message_template = \
+  '\t<context>\n' \
+  '\t\t<name>' + context_name + '</name>\n'
+
+  for msg in dico.values():
+    for msg_item in msg:
+      relative_path = get_relative_path(source_file, msg_item['filename'])
+      message_template += \
+        '\t\t<message>\n' \
+            '\t\t\t<location filename="' + relative_path + '" line="' + str(msg_item['line']) + '"/>\n' \
+            '\t\t\t<source><![CDATA[' + msg_item['source'] + ']]></source>\n' \
+            '\t\t\t<translation type="unfinished"></translation>\n' \
+        '\t\t</message>\n'
+  message_template +=  '\t</context>\n'
+
+  return message_template
+
+def dict_to_ts(dico, source_file, filename="slicer.ts"):
+  if not dico:
+    print("Error : the specified file doesn't contain any marked string")
+    return
+
+  tsfile_start_template = \
+    '<?xml version="1.0" encoding="utf-8"?>\n' \
+    '<!DOCTYPE TS>\n' \
+    '<TS version="2.1">\n'
+
+  tsfile_end_template = '</TS>'
+
+  with open(filename, "w") as f:
+    f.write(tsfile_start_template)
+    message_template = ''
+    if not is_big(dico):
+      context_name = get_context(source_file)
+      message_template = ts_context_item(context_name, source_file, dico)
+    else:
+      for py_file in dico.keys():
+        minidico = dico[py_file]
+        if minidico:
+          context_name = get_context(py_file)
+          message_template += ts_context_item(context_name, source_file, minidico)
+    f.write(message_template)
+    f.write(tsfile_end_template)
 
 def ts_to_dict(tsfile):
   """ transform .ts file containing extracted strings to a dictionnary"""
@@ -124,18 +128,18 @@ def dict_values_length(dico):
 
 def get_context(source_file):
   """ get context name based on source code file/folder"""
-  context_name = ""
   if os.path.isfile(source_file):
-    with open(source_file, "r") as code:
-      tree = ast.parse(code.read())
-      classes = [node.name for node in walk(tree) if isinstance(node, ast.ClassDef)]
-      path = pathlib.PurePath(os.path.realpath(source_file))
-      package_name = str(path.parent.name)
-      if len(classes)!=0: context_name = package_name+"."+classes[0]
-      else: context_name = source_file[:-3]
-  elif os.path.isdir(source_file):
-    context_name = source_file
-  return context_name
+    parent_folder = os.path.dirname(source_file)
+    init_file_path = parent_folder + os.path.sep + '__init__.py'
+
+    if os.path.isfile(init_file_path):
+      context_name = os.path.basename(parent_folder)
+      context_name += '.' + os.path.basename(source_file).replace('.py', '');
+      return context_name
+    else:
+      return os.path.basename(source_file).replace('.py', '')
+  else:
+    return os.path.basename(source_file)
 
 def get_all_py_file(folder):
   """get all .py file in a folder"""
@@ -207,3 +211,14 @@ def walk (node):
             continue
         end += walk(n)
     return end
+
+def get_relative_path(root_path, full_path):
+  root_path = os.path.abspath(root_path)
+  full_path = os.path.abspath(full_path)
+
+  relative_path = full_path.replace(root_path, '').replace('\\', '/')
+  relative_path = relative_path[1:] if relative_path.startswith('/') else relative_path
+
+  # print('rel', relative_path, '=>', root_path, ':', full_path)
+
+  return relative_path if relative_path != '' else full_path
